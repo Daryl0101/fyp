6cfdee410ed406180a8352c54e0011126cb09c12
Daryl0101 <daryl.tneoh@gmail.com>
2024-06-05 13:38:58 +0800

    fix(settings): move django secret key to env

--- food_allocation/food_allocation/settings.py
+++ food_allocation/food_allocation/settings.py
@@ -22,7 +22,7 @@ from celery.schedules import crontab
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^j-ym1-vb^z1l#rv2wn#nq2a#0d2dy_nu=nz@t*nl3hin*i!^5"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
